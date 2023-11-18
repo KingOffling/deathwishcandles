1ad584acdc84ccb92d2d2d4a816b961dc8560219
KingOffling <125208496+KingOffling@users.noreply.github.com>
2023-11-18 09:24:28 -0500

    okok

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React, { useState, useEffect } from 'react';
-import { Button, Text, VStack, ChakraProvider, extendTheme, Image } from '@chakra-ui/react';
+import React, { useState, useEffect, useCallback } from 'react';
+import {
+  Button, Text, VStack, ChakraProvider, extendTheme, Image, HStack,
+  Modal, ModalOverlay, ModalContent, ModalHeader, ModalBody, ModalCloseButton, useDisclosure
+} from '@chakra-ui/react';
 import { ethers } from 'ethers';
 import candlesABI from './candlesABI.json';
 import skullsABI from './skullsABI.json';
@@ -11,12 +14,8 @@ import './App.css';
 
 const theme = extendTheme({
   colors: {
-    red: {
-      500: '#E53E3E',
-    },
-    black: {
-      500: '#000000',
-    },
+    red: { 500: '#E53E3E' },
+    black: { 500: '#000000' },
   },
 });
 
@@ -24,42 +23,32 @@ function App() {
   const [isWalletConnected, setIsWalletConnected] = useState(false);
   const [userAddress, setUserAddress] = useState('');
   const provider = new ethers.providers.Web3Provider(window.ethereum);
-  // eslint-disable-next-line no-unused-vars
   const signer = provider.getSigner();
-
   const candlesAddress = "0x521945fDCEa1626E056E89A3abBDEe709cf3a837";
   const skullsAddress = "0x67e3e965ce5ae4d6a49ac643205897acb32fcf6e";
-
   const candlesContract = new ethers.Contract(candlesAddress, candlesABI, provider);
   const skullsContract = new ethers.Contract(skullsAddress, skullsABI, provider);
-
   const [candleQuantities, setCandleQuantities] = useState({ 1: 0, 2: 0, 3: 0 });
 
-
-  const skullSize = 55; // Size of each skull including gap
+  const skullSize = 55;
   const skullsPerRow = Math.floor(window.innerWidth / skullSize);
   const rowsPerScreen = Math.floor(window.innerHeight / skullSize);
-  const initialSkullsCount = skullsPerRow * rowsPerScreen;
+  const initialSkullsCount = 365;
   const [skullsLoaded, setSkullsLoaded] = useState(initialSkullsCount);
 
 
-
-  //#region QUERY TOKENS
-  /////////////////////////
   useEffect(() => {
     if (userAddress) {
-      queryCandles(provider);
+      queryCandles();
     }
-      // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [userAddress]);
 
-  const queryCandles = async (provider) => {
+  const queryCandles = async () => {
     try {
       let quantities = { 1: 0, 2: 0, 3: 0 };
       for (let id = 1; id <= 3; id++) {
         const balance = await candlesContract.balanceOf(userAddress, id);
         quantities[id] = parseInt(balance.toString(), 10);
-        console.log(`Candle ID ${id} Balance:`, balance.toString());
       }
       setCandleQuantities(quantities);
     } catch (error) {
@@ -67,42 +56,17 @@ function App() {
     }
   };
 
-  // eslint-disable-next-line no-unused-vars
-  const querySkull = async (provider, tokenID) => {
-    const maxId = 365; // or use totalSupply if available
-    for (let id = 1; id <= maxId; id++) {
-      try {
-        const owner = await skullsContract.ownerOf(id);
-        if (owner.toLowerCase() === userAddress.toLowerCase()) {
-          console.log(`You own Skull ID: ${id}`);
-        }
-      } catch (error) {
-        if (error.code === 'CALL_EXCEPTION') {
-          //          console.log(`Skull ID ${id} does not exist or other error`);
-        } else {
-          console.error("Unexpected error querying skulls:", error);
-          break; // Optional: break out of the loop if a non-expected error occurs
-        }
-      }
-    }
-  };
-  //#endregion
-
-  //#region WALLET CONNECTOR
-  /////////////////////////
-
-  // wallet updator
   useEffect(() => {
     const handleAccountsChanged = async (accounts) => {
       if (accounts.length === 0) {
         setIsWalletConnected(false);
         setUserAddress('');
-        setCandleQuantities({ 1: 0, 2: 0, 3: 0 }); // Reset candle quantities
+        setCandleQuantities({ 1: 0, 2: 0, 3: 0 });
       } else {
         const newAddress = accounts[0];
         setUserAddress(newAddress);
         setIsWalletConnected(true);
-        await queryCandles(provider); // Fetch candles for the new address
+        await queryCandles();
       }
     };
 
@@ -110,65 +74,47 @@ function App() {
       window.ethereum.on('accountsChanged', handleAccountsChanged);
     }
 
-    // Clean up the event listener when the component unmounts
     return () => {
       if (window.ethereum) {
         window.ethereum.removeListener('accountsChanged', handleAccountsChanged);
       }
     };
-      // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); // This effect does not depend on any state or props
+  }, []);
 
-
-
-
-  // wallet connector
   const connectWallet = async () => {
-    try {
-      if (!window.ethereum) {
-        alert('Please install MetaMask!');
-        return;
-      }
-
-      // Request account access
-      const provider = new ethers.providers.Web3Provider(window.ethereum);
-      await provider.send("eth_requestAccounts", []);
-      const signer = provider.getSigner();
-
-      // Get the user's address
-      const address = await signer.getAddress();
-      console.log('Connected with address:', address);
-
-      // Set state
-      setIsWalletConnected(true);
-      setUserAddress(address);
-    } catch (error) {
-      console.error(error);
-      alert('An error occurred!');
-      setIsWalletConnected(false);
-      setUserAddress('');
+    if (!window.ethereum) {
+      alert('Please install MetaMask!');
+      return;
     }
-  }
 
+    await provider.send("eth_requestAccounts", []);
+    const address = await signer.getAddress();
 
+    setIsWalletConnected(true);
+    setUserAddress(address);
+  };
 
-  //#endregion
+  const [selectedCandle, setSelectedCandle] = useState(null);
 
-  //#region SHOW CANDLES
-  ////////////////////////////
-  const DisplayCandles = ({ candleQuantities }) => {
+  const handleCandleClick = useCallback((candleId) => {
+    setSelectedCandle(candleId);
+  }, []);
+
+  const DisplayCandles = React.memo(({ candleQuantities }) => {
+    const totalCandles = Object.values(candleQuantities).reduce((acc, quantity) => acc + quantity, 0);
+
     const getCandleImage = (tokenId) => {
       switch (tokenId) {
-        case 1:
-          return rareCandle;
-        case 2:
-          return uncommonCandle;
-        case 3:
-          return commonCandle;
-        default:
-          return null;
+        case 1: return rareCandle;
+        case 2: return uncommonCandle;
+        case 3: return commonCandle;
+        default: return null;
       }
     };
+
+    if (totalCandles === 0) {
+      return <Text color="grey" fontSize="x-large">No Candles Present</Text>;
+    }
 
     return (
       <div className="candle-container">
@@ -177,123 +123,92 @@ function App() {
             <img
               key={`${tokenId}-${index}`}
               src={getCandleImage(parseInt(tokenId, 10))}
-              alt="Candle"
-              className="candle-image"
+              alt={`Candle ${tokenId}`}
+              className={`candle-image ${selectedCandle === tokenId ? 'selected' : ''}`}
+              onClick={() => handleCandleClick(tokenId)}
             />
           ))
         )}
       </div>
     );
-  };
-  //#endregion
-
-  //#region SCROLLING FOREVER
-  ////////////////////////////
-  useEffect(() => {
-    const handleScroll = () => {
-      if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
-        setSkullsLoaded(skullsLoaded => skullsLoaded + skullsPerRow);
-      }
-    };
-  
-    window.addEventListener('scroll', handleScroll);
-  
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, [skullsPerRow]);
-  
-
-
-  //endregion
-
-  //#region SHOW SKULLS
-  //////////////////////////
-  const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-  }
-
-  // eslint-disable-next-line no-unused-vars
-  const loadMoreSkulls = () => {
-    setSkullsLoaded(current => current + 19); // Load another row of skulls
-  };
+  });
 
   const ScrollingSkullsGrid = () => {
-    const createSkullImages = (count) => {
-      let skulls = [];
-      for (let i = 1; i <= count; i++) {
+    const [allSkulls, setAllSkulls] = useState([]);
+    const { isOpen, onOpen, onClose } = useDisclosure();
+    const [selectedSkullId, setSelectedSkullId] = useState(null);
+
+    useEffect(() => {
+      let newSkulls = [];
+      for (let i = 1; i <= skullsLoaded; i++) {
         let formattedId = String(((i - 1) % 365) + 1).padStart(3, '0');
         let imgSrc = `/images/skulls/DW365-${formattedId}.jpg`;
-        skulls.push(
-          <img
-            key={i}
-            src={imgSrc}
-            alt={`Skull ${i}`}
-            className="skull-image"
-          />
+        newSkulls.push(
+          <img key={i} src={imgSrc} alt={`Skull ${i}`} className="skull-image"
+            onClick={() => { setSelectedSkullId(formattedId); onOpen(); }} />
         );
       }
-      return shuffleArray(skulls);
-    };
+      setAllSkulls(newSkulls);
+    }, [skullsLoaded, onOpen]);
 
     return (
-      <div className="scrolling-skulls-grid">
-      {createSkullImages(skullsLoaded)}
-    </div>
+      <div>
+        <div className="scrolling-skulls-grid">
+          {allSkulls}
+        </div>
+        {selectedSkullId && (
+          <Modal isOpen={isOpen} onClose={onClose}>
+            <ModalOverlay />
+            <ModalContent>
+              <ModalHeader>DW365-{selectedSkullId}</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Image src={`/images/skulls/DW365-${selectedSkullId}.jpg`} width="400px" height="400px" />
+              </ModalBody>
+            </ModalContent>
+          </Modal>
+        )}
+      </div>
     );
   };
-  // #endregion
 
+  // This useEffect hook has been removed
+  // useEffect(() => {
+  //   const handleScroll = () => {
+  //     if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 100) {
+  //       setSkullsLoaded(prev => prev + skullsPerRow);
+  //     }
+  //   };
+  //   window.addEventListener('scroll', handleScroll);
+  //   return () => window.removeEventListener('scroll', handleScroll);
+  // }, [skullsPerRow]);
 
-
-
-
-
-
-
-
-
-  //#region SHOWTIME
-  /////////////////////////
   return (
     <ChakraProvider theme={theme}>
       <VStack spacing={4} align="center" justify="center" minHeight="100vh" bgColor="black.500">
-        <Image src={logo} width="400px" marginTop={"50px"}></Image>
-        {isWalletConnected
-          && candleQuantities && <Text color="white" fontSize="x-large">Select Your Candle</Text>}
+        <Image src={logo} width="400px" marginTop={"50px"} />
+        {isWalletConnected && candleQuantities && <Text color="white" fontSize="x-large">Select Your Candle</Text>}
         {candleQuantities && <DisplayCandles candleQuantities={candleQuantities} />}
         <Button
           colorScheme="red"
-          onClick={!isWalletConnected ? connectWallet : null}
+          onClick={!isWalletConnected ? connectWallet : () => {/* perform ritual action */}}
           m={4}
         >
-          {isWalletConnected ? 'Connected' : 'Connect Wallet'}
+          {selectedCandle ? 'Perform Ritual' : 'Connect Wallet'}
         </Button>
-        {isWalletConnected ? (
-          <Text color="white" fontSize="small" opacity={.5}>
-            {`${userAddress}`}
-          </Text>
-        ) : (
-          <Text color="white" fontSize="small" opacity={0}>
-            .
-          </Text>
-        )}
-        return (
-        <div>
+        {isWalletConnected ? <Text color="white" fontSize="small" opacity={.5}>{userAddress}</Text> : <Text color="white" fontSize="small" opacity={0}>.</Text>}
         <ScrollingSkullsGrid />
-        </div>
-        );
-
-
+          <HStack spacing={4} m={"20px"}>
+            <a href="https://twitter.com/deathwishnft" target="_blank" rel="noopener noreferrer">Twitter</a>
+            <a href="https://opensea.io/deathwish-365" target="_blank" rel="noopener noreferrer">OpenSea</a>
+            <a href="https://discord.gg/deathwishnft" target="_blank" rel="noopener noreferrer">OpenSea</a>
+          </HStack>
       </VStack>
+      
+          
+      
     </ChakraProvider>
   );
-  //#endregion
-
 }
 
 export default App;
