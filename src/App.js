9adca7aae409fb16c7c06a799ec2fed1a3aaa2dd
KingOffling <125208496+KingOffling@users.noreply.github.com>
2023-12-09 14:31:19 -0500

    okok

--- src/App.js
+++ src/App.js
@@ -30,6 +30,12 @@ function App() {
   const [buttonText, setButtonText] = useState('Loading...');
   const [buttonColor, setButtonColor] = useState('grey');
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [displayAddress, setDisplayAddress] = useState('');
+  const [skullDescription, setSkullDescription] = useState('');
+  const [quoteAuthor, setQuoteAuthor] = useState('');
+  const [mintedDate, setMintedDate] = useState('');
+  const [prestigeStatus, setPrestigeStatus] = useState('');
+
 
 
   const readOnlyProvider = new ethers.providers.JsonRpcProvider("https://mainnet.infura.io/v3/edd1ef15a39f46a495d9441c6bdb9c45");
@@ -49,7 +55,6 @@ function App() {
   );
 
 
-
   // #region Wallet Connectivity
   const connectWallet = async () => {
     if (!window.ethereum) {
@@ -61,6 +66,8 @@ function App() {
     const address = await provider.getSigner().getAddress();
     setIsWalletConnected(true);
     setUserAddress(address);
+    const ensName = await getEnsName(address);
+    setDisplayAddress(ensName || formatAddress(address));
   };
 
 
@@ -74,6 +81,9 @@ function App() {
     }
   };
   
+  const formatAddress = (address) => {
+    return `${address.substring(0, 6)}...${address.substring(address.length - 4)}`;
+  };
 
   // #endregion
 
@@ -144,7 +154,15 @@ function App() {
     setIsModalOpen(true); // Open modal
     setButtonText('Loading...');
     setButtonColor('grey');
-    await checkTokenOwnership(formattedId); // Fetch new data
+    await checkTokenOwnership(formattedId);
+
+    const metadata = await fetchSkullMetadata(formattedId);
+  if (metadata) {
+    setSkullDescription(metadata.description);
+    setQuoteAuthor(metadata.quoteAuthor);
+    setMintedDate(formatDate(metadata.mintedDate));
+    setPrestigeStatus(metadata.prestigeStatus);
+  }
   };
 
 
@@ -206,7 +224,9 @@ function App() {
       />
     );
 
+// #endregion
 
+ // #region Skull Grid Modal
 
     return (
       <div>
@@ -226,21 +246,64 @@ function App() {
                 mb={"-5px"}
               >
                 DW365-{selectedSkullId.padStart(3, '0')}
+                <Text style={getPrestigeStatusStyle(prestigeStatus)} align={"center"} fontSize={"26px"} mb={"-14px"} mt={"-14px"}>
+                  {prestigeStatus}
+                </Text>
               </ModalHeader>
 
               <ModalBody display="flex" flexDirection="column" justifyContent="center" style={{ paddingBottom: '25px' }}>
-                <Image
-                  src={`/images/skulls/DW365-${selectedSkullId}.jpg`}
-                  alt={`Skull ${selectedSkullId}`}
-                  boxSize="400px"
-                />
+                <div className="image-overlay-container">
+                  <Image
+                    src={`/images/skulls/DW365-${selectedSkullId}.jpg`}
+                    alt={`Skull ${selectedSkullId}`}
+                    boxSize="400px"
+                    border="2px solid black"
+                  />
+<div className="overlay-text">
+  <Text
+    style={{
+      color: "white",
+      fontSize: calculateFontSize(),
+      overflowWrap: "break-word",
+      textAlign: "left",
+      padding: "10px",
+      marginBottom: "auto", // Pushes the author to the bottom
+    }}
+    className="description-text"
+  >
+    {skullDescription}
+  </Text>
+  <Text
+    style={{
+      color: "white",
+      fontSize: calculateFontSize(),
+      overflowWrap: "break-word",
+      textAlign: "right",
+      padding: "10px"
+    }}
+    className="author-text"
+  >
+    {quoteAuthor}
+  </Text>
+</div>
+
+
+                </div>
+                <Text color="black" fontFamily={"Arial"} fontSize="12px">
+                  {mintedDate}
+                </Text>
                 <Button
-                  style={{ backgroundColor: buttonColor, color: 'white' }}
+                  style={{ 
+                    backgroundColor: buttonColor, 
+                    color: 'white', 
+                    padding: '4px 8px'
+                  }}
                   width={"60%"}
                   mt="10px"
                   alignSelf={"center"}
                   border={"1px"}
                   borderColor={"black"}
+                  onClick={handleButtonClick}
                 >
                   {buttonText}
                 </Button>
@@ -257,17 +320,90 @@ function App() {
 
   //#endregion
 
+  // #region Button Pushing
+  
+  const handleButtonClick = () => {
+    const skullIdNumber = parseInt(selectedSkullId, 10);
+    const url = `https://opensea.io/assets/ethereum/0x67e3e965ce5ae4d6a49ac643205897acb32fcf6e/${skullIdNumber}`;
+    window.open(url, '_blank');
+  };
+  
+  
+  // #endregion
 
+  // #region Metadata Collection
+
+    const formatDate = (unixTimestamp) => {
+      const date = new Date(unixTimestamp * 1000);
+      return `${date.getDate().toString().padStart(2, '0')}/${(date.getMonth() + 1).toString().padStart(2, '0')}/${date.getFullYear()}`;
+    };
+    
+    const fetchSkullMetadata = async (id) => {
+      const skullIdNumber = parseInt(id, 10);
+      const metadataUrl = `https://metadata.deathwishnft.io/metadata/${skullIdNumber}`;
+      try {
+        const response = await fetch(metadataUrl);
+        const metadata = await response.json();
+        return {
+          description: metadata.description,
+          quoteAuthor: metadata.attributes.find(attr => attr.trait_type === "Quote Author")?.value,
+          mintedDate: metadata.attributes.find(attr => attr.trait_type === "Minted")?.value,
+          prestigeStatus: metadata.attributes.find(attr => attr.trait_type === "Prestige Status")?.value,
+        };
+      } catch (error) {
+        console.error("Error fetching metadata:", error);
+        return null;
+      }
+    };
+
+    const getPrestigeStatusStyle = (status) => {
+      const baseStyle = { fontFamily: "Rockledge, sans-serif",
+      letterSpacing: "2px"};   
+      switch (status) {
+        case "Mythic":
+          return { ...baseStyle, color: "#e69a0e" };
+        case "Epic":
+          return { ...baseStyle, color: "#ba03fc" };
+        case "Rare":
+          return { ...baseStyle, color: "#39c0e0" };
+        case "Uncommon":
+          return { ...baseStyle, color: "#66f28b" };
+        case "Common":
+        case "Undetermined":
+          return { ...baseStyle, color: "#c7c7c7" };
+        default:
+          return baseStyle;
+      }
+    };
+    
+    const calculateFontSize = () => {
+      const combinedLength = skullDescription.length + quoteAuthor.length;
+      
+      if (combinedLength < 50) {
+        return '48px'; 
+      } else if (combinedLength < 100) {
+        return '40px'; 
+      } else {
+        return '32px'; 
+      }
+    };
+    
+
+  // #endregion
 
   // #region Live Updates / Effects
   useEffect(() => {
     const handleAccountsChanged = async (accounts) => {
       setIsWalletConnected(accounts.length > 0);
-      setUserAddress(accounts[0] || '');
+      const address = accounts[0] || '';
+      setUserAddress(address);
       if (accounts.length > 0) {
         await queryCandles();
+        const ensName = await getEnsName(address);
+        setDisplayAddress(ensName || formatAddress(address));
       } else {
         setCandleQuantities({ 1: 0, 2: 0, 3: 0 });
+        setDisplayAddress('');
       }
     };
 
@@ -280,13 +416,16 @@ function App() {
         window.ethereum.removeListener('accountsChanged', handleAccountsChanged);
       }
     };
-  }, [queryCandles]);
+  }, [queryCandles, getEnsName]);
   // #endregion
 
   return (
     <ChakraProvider theme={theme}>
       <VStack spacing={4} align="center" justify="center" minHeight="100vh" bgColor="black.500">
         <Image src={logo} width="400px" marginTop={"50px"} />
+        <Text color="white" fontFamily={"Rockledge"} fontSize="2em" mb={"-1em"} opacity={1}>Light Your Candle</Text>
+        <Text color="white" fontFamily={"Rockledge"} fontSize="2em" mb={"-1em"} opacity={1}>Complete the ritual</Text>
+        <Text color="white" fontFamily={"Rockledge"} fontSize="2em" mb={"1em"} opacity={1}>Define Prestige</Text>
         {isWalletConnected && <DisplayCandles />}
         <Button
           colorScheme="red"
@@ -296,7 +435,7 @@ function App() {
           {selectedCandle ? 'Perform Ritual' : 'Connect Wallet'}
         </Button>
         {isWalletConnected ?
-          <Text color="white" fontSize="small" opacity={.5}>{userAddress}</Text> :
+          <Text color="white" fontSize="small" opacity={.5}>{displayAddress}</Text> :
           <Text color="white" fontSize="small" opacity={0}>.</Text>
         }
             <ScrollingSkullsGrid
